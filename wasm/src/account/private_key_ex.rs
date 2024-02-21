39a412d64124241ce6de75865d02c7d18cd6b17b
OzielLa <oziel.la@gmail.com>
2024-02-21 14:38:14 +0800

    add serialnumberrecords

--- wasm/src/account/private_key_ex.rs
+++ wasm/src/account/private_key_ex.rs
@@ -14,7 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with the Aleo SDK library. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{account::ViewKey, record::RecordCiphertext, types::native::RecordPlaintextNative as Record, PrivateKey};
+use crate::{
+    account::ViewKey,
+    record::RecordCiphertext,
+    types::native::RecordPlaintextNative as Record,
+    PrivateKey,
+    RecordPlaintext,
+};
 
 use core::ops::Deref;
 use wasm_bindgen::prelude::*;
@@ -23,7 +29,7 @@ use anyhow::Result as AnyhowResult;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
 pub struct RecordData {
     pub record: Record,
     pub serial_number: String,
@@ -68,6 +74,34 @@ impl PrivateKey {
             .unwrap_or_default();
         Ok(serde_json::to_string_pretty(&decrypted_records).unwrap_or_default().replace("\\n", ""))
     }
+
+    #[wasm_bindgen(js_name = "serialnumberrecords")]
+    pub fn serial_number_records(&self, recordstext: &str) -> Result<String, String> {
+        let record_org_datas: Vec<RecordData> = serde_json::from_str(recordstext).unwrap_or_default();
+        let decrypted_records = record_org_datas
+            .iter()
+            .filter_map(|record_org_data| serial_number_record(self, record_org_data).transpose())
+            .collect::<Result<Vec<RecordData>, _>>()
+            .unwrap_or_default();
+        Ok(serde_json::to_string_pretty(&decrypted_records).unwrap_or_default().replace("\\n", ""))
+    }
+}
+
+pub fn serial_number_record(private_key: &PrivateKey, record_org: &RecordData) -> AnyhowResult<Option<RecordData>> {
+    let program_id = &record_org.record_meta.program_id;
+    let record_name = &record_org.record_meta.identifier;
+
+    if let Ok(serial_number) =
+        RecordPlaintext::from(record_org.record.clone()).serial_number_string(private_key, program_id, record_name)
+    {
+        let record_data: RecordData = RecordData {
+            record: record_org.record.clone(),
+            serial_number,
+            record_meta: record_org.record_meta.clone(),
+        };
+        return Ok(Some(record_data));
+    };
+    Ok(None)
 }
 
 pub fn decrypt_record_data(
